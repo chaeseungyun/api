1e7a1bfaac5a0d4fb198d186b5c07baf38205b92
chaeseungyun <101871802+chaeseungyun@users.noreply.github.com>
2022-05-23 15:48:25 +0900

    new
    new

--- api.js
+++ api.js
@@ -2,20 +2,19 @@
 let button = document.getElementsByClassName('btn');
 let lat = [36.814, 35.1795543, 37.413294];
 let lon = [127.15, 129.0756416, 127.0016985];
-for(let i=0;i<button.length;i++){
-    button[i].onclick = function() {find(lat[i],lon[i]);}
-}
 const temp = document.getElementById('temp');
 const rising = document.getElementById('rising');
 const weather = document.getElementById('weather');
 const falling = document.getElementById('falling');
+const img = document.getElementById('img');
 
+for(let i=0;i<button.length;i++){
+    button[i].onclick = function() {find(lat[i],lon[i]);}
+}
 function find(lat, lon) {axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0be87df02cd14b1d08489cd0372b5b0e&lang=kr`)
 .then((response)=>weatherinfo(response))
 .catch((e)=>console.log(e))}
-//35.1795543
-//129.0756416
-
+function addicon(icon){img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`} 
 function weatherinfo(res) {
     temp.innerHTML=res.data.main.temp-273.15;
     weather.innerHTML=res.data.weather[0].description;
@@ -26,4 +25,5 @@ function weatherinfo(res) {
     rising.innerHTML=sunrise;
     falling.innerHTML=sunset;
     console.log(res.data.weather[0].description);
+    addicon(res.data.weather[0].icon);   
 }
